51d91d05c36c7ec91d588052b7c6b2025bcee59b
Alexis Renard <renard.alexis@hotmail.fr>
2016-05-30 13:38:20 +0200

    add modifs to register

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#login").click(function() {
+    $("#login").click(function() {
         display_login();
     });
     $("#register").click(function() {
@@ -107,8 +107,8 @@ function display_register(){
     html +="        <div class=\"row\">";
     html +="          <div class=\"col-md-12 col-sm-12 col-xs-12\">";
     html +="                  <div class=\"feature_header text-center\">";
-    html +="                      <h3 class=\"feature_title\">Sign <b>in</b></h3>";
-    html +="                      <h4 class=\"feature_sub\">Take your seats! There's enough cake for everybody!  </h4>";
+    html +="                      <h3 class=\"feature_title\">Sign <b>up</b></h3>";
+    html +="                      <h4 class=\"feature_sub\">Fresh meat is near...</h4>";
     html +="                      <div class=\"divider\"></div>";
     html +="                  </div>";
     html +="          </div>";
@@ -119,16 +119,28 @@ function display_register(){
     html +="                <div class=\"left_contact\">"
     html +="                    <form action=\"role\">"
     html +="                        <div class=\"form-level\">"
-    html +="                            <input name=\"name\" placeholder=\"Name\" id=\"name\"  value=\"\" type=\"text\" class=\"input-block\">"
+    html +="                            <input name=\"username\" placeholder=\"Username\" id=\"username\"  value=\"\" type=\"text\" class=\"input-block\">"
     html +="                            <span class=\"form-icon fa fa-user\"></span>"
     html +="                        </div>"
     html +="                        <div class=\"form-level\">"
-    html +="                            <input name=\"email\" placeholder=\"Email\" id=\"mail\" class=\"input-block\" value=\"\" type=\"email\">"
-    html +="                            <span class=\"form-icon fa fa-envelope-o\"></span>"
+    html +="                            <input name=\"password\" placeholder=\"Password\" id=\"password\" class=\"input-block\" value=\"\" type=\"password\">"
+    html +="                            <span class=\"form-icon fa fa-key\"></span>"
     html +="                        </div>"
     html +="                        <div class=\"form-level\">"
-    html +="                            <input name=\"name\" placeholder=\"Phone\" id=\"phone\" class=\"input-block\" value=\"\" type=\"text\">"
-    html +="                            <span class=\"form-icon fa fa-phone\"></span>"
+    html +="                            <input name=\"firstName\" placeholder=\"First Name\" id=\"firstName\" class=\"input-block\" value=\"\" type=\"firstName\">"
+    html +="                            <span class=\"form-icon fa fa-hand-spock-o\"></span>"
+    html +="                        </div>"
+    html +="                        <div class=\"form-level\">"
+    html +="                            <input name=\"lastName\" placeholder=\"Last Name\" id=\"lastName\" class=\"input-block\" value=\"\" type=\"lastName\">"
+    html +="                            <span class=\"form-icon fa fa-hand-paper\"></span>"
+    html +="                        </div>"
+    html +="                        <div class=\"form-level\">"
+    html +="                            <input name=\"email\" placeholder=\"Email\" id=\"mail\" class=\"input-block\" value=\"\" type=\"email\">"
+    html +="                            <span class=\"form-icon fa fa-envelope\"></span>"
+    html +="                        </div>"
+    html +="                        <div class=\"form-level\">"
+    html +="                            <input name=\"email\" placeholder=\"Email\" id=\"mail\" class=\"input-block\" value=\"\" type=\"email\">"
+    html +="                            <span class=\"form-icon fa fa-envelope\"></span>"
     html +="                        </div>"
     html +="                    </form>"
     html +="                </div>"
@@ -136,7 +148,7 @@ function display_register(){
     html +=""
     html +="            <div class=\"col-md-6 right\">"
     html +="                <div class=\"form-level\">"
-    html +="                    <textarea name=\"desc\" id=\"desc\"  rows=\"5\" class=\"textarea-block\" placeholder=\"Description\"></textarea>"
+    html +="                    <textarea name=\"desc\" id=\"desc\"  rows=\"10\" class=\"textarea-block\" placeholder=\"Description\"></textarea>"
     html +="                    <span class=\"form-icon fa fa-pencil\"></span>"
     html +="                </div>"
     html +="            </div>"
